8d12b05785ebce946473df86e6f65db471a308e6
Igor <igor.m99@hotmail.com>
2021-11-24 19:50:52 -0300

    Questão 4 terminada. Mexer no front

--- API/Controllers/VendaController.cs
+++ API/Controllers/VendaController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using API.Data;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,13 +16,29 @@ namespace API.Controllers
             _context = context;
         }
 
+        //POST: api/venda/create
+        [HttpPost]
+        [Route("create")]
+
+        public IActionResult Create([FromBody] Venda venda)
+        {
+
+            _context.Vendas.Add(venda);
+            _context.SaveChanges();
+            return Created("", venda);
+        }
+
         //GET: api/venda/list
-        //ALTERAR O MÉTODO PARA MOSTRAR TODOS OS DADOS DA VENDA E OS DADOS RELACIONADOS
         [HttpGet]
         [Route("list")]
         public IActionResult List()
         {
-            return Ok(_context.Vendas.ToList());
+            return Ok(_context.Vendas
+            .Include(c => c.Itens)
+            .ThenInclude(x => x.Produto)
+            .ThenInclude(y => y.Categoria)
+            .ToList()
+            );
         }
     }
 }
